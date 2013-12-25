ec1bc3dea2b9265cae1708047c87959c662fd101
Wesley De Groot <sales@wdgp.nl>
2013-12-25 20:28:02 +0100

    Fixed a lot of crap!

--- gen.php
+++ gen.php
@@ -58,22 +58,49 @@ $dir = opendir('items');
 
 $template = file_get_contents("http://www.wdgwv.com/placeholder");
 
+$menu = array();
+
 while ( ( $file = readdir ( $dir ) ) !== false )
 {
 	if ($file != '.' && $file != '..')
 	{
 		echo "Generating " . $file . "\r\n";
+
 		$open = file_get_contents('items/' . $file);
-		$open = preg_replace("#{BACK}#", "<a href='/index.html'>Back</a><hr />", $open);
+		$open = preg_replace("#{BACK}#", "<a href='/index.html'>Back</a><hr /><br /><br />", $open);
 
 		$replace = $template;
 		$replace = preg_replace("#PLACEHOLDER#", $open, $replace);
+
+		$menu[] = substr($file, 0, -5);
 
 		$file = fopen($file, 'w');
 		@fwrite($file, $replace);
 		@fclose($file);
 	}
 }
+
+#FIX THE MENU
+	$open    = file_get_contents('items/index.html');
+	$open    = preg_replace("#{BACK}#", "<a href='/index.html'>Back</a><hr /><br /><br />", $open);
+
+	$menuTMP = null;
+	for ($i=0; $i<sizeof($menu); $i++) 
+	{ 
+	
+		$menuTMP .= "<a href=\"{$menu[$i]}.html\">{$menu[$i]}</a>";
+
+		if ($i < (sizeof($menu)-1) )
+			$menuTMP .= "&nbsp;&nbsp;||&nbsp;&nbsp;";
+	}
+
+	$open    = preg_replace("#{MENU}#", $menuTMP, $open);
+	$replace = $template;
+	$replace = preg_replace("#PLACEHOLDER#", $open, $replace);
+
+	$file = fopen('index.html', 'w');
+	@fwrite($file, $replace);
+	@fclose($file);
 
 echo "All Files Created!\r\n\tDon't Wait!!!\r\n\r\n\t\tUPLOAD IT!!!";
 ?>
