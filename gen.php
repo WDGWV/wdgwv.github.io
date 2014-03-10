a51e97977b4eeb8e034f0a4ca6e1c97cf7290907
Wesley De Groot <sales@wdgp.nl>
2014-03-10 21:32:27 +0100

    FUCK YOU GITHUB!

---
+++ gen.php
@@ -0,0 +1,118 @@
+<?php
+/*
+                   :....................,:,              
+                ,.`,,,::;;;;;;;;;;;;;;;;:;`              
+              `...`,::;:::::;;;;;;;;;;;;;::'             
+             ,..``,,,::::::::::::::::;:;;:::;            
+            :.,,``..::;;,,,,,,,,,,,,,:;;;;;::;`          
+           ,.,,,`...,:.:,,,,,,,,,,,,,:;:;;;;:;;          
+          `..,,``...;;,;::::::::::::::'';';';:''         
+          ,,,,,``..:;,;;:::::::::::::;';;';';;'';        
+         ,,,,,``....;,,:::::::;;;;;;;;':'''';''+;        
+         :,::```....,,,:;;;;;;;;;;;;;;;''''';';';;       
+        `,,::``.....,,,;;;;;;;;;;;;;;;;'''''';';;;'      
+        :;:::``......,;;;;;;;;:::::;;;;'''''';;;;:       
+        ;;;::,`.....,::;;::::::;;;;;;;;'''''';;,;;,      
+        ;:;;:;`....,:::::::::::::::::;;;;'''':;,;;;      
+        ';;;;;.,,,,::::::::::::::::::;;;;;''':::;;'      
+        ;';;;;.;,,,,::::::::::::::::;;;;;;;''::;;;'      
+        ;'';;:;..,,,;;;:;;:::;;;;;;;;;;;;;;;':::;;'      
+        ;'';;;;;.,,;:;;;;;;;;;;;;;;;;;;;;;;;;;:;':;      
+        ;''';;:;;.;;;;;;;;;;;;;;;;;;;;;;;;;;;''';:.      
+        :';';;;;;;::,,,,,,,,,,,,,,:;;;;;;;;;;'''';       
+         '';;;;:;;;.,,,,,,,,,,,,,,,,:;;;;;;;;'''''       
+         '''';;;;;:..,,,,,,,,,,,,,,,,,;;;;;;;''':,       
+         .'''';;;;....,,,,,,,,,,,,,,,,,,,:;;;''''        
+          ''''';;;;....,,,,,,,,,,,,,,,,,,;;;''';.        
+           '''';;;::.......,,,,,,,,,,,,,:;;;''''         
+           `''';;;;:,......,,,,,,,,,,,,,;;;;;''          
+            .'';;;;;:.....,,,,,,,,,,,,,,:;;;;'           
+             `;;;;;:,....,,,,,,,,,,,,,,,:;;''            
+               ;';;,,..,.,,,,,,,,,,,,,,,;;',             
+                 '';:,,,,,,,,,,,,,,,::;;;:               
+                  `:;'''''''''''''''';:.                 
+                                                         
+ ,,,::::::::::::::::::::::::;;;;,::::::::::::::::::::::::
+ ,::::::::::::::::::::::::::;;;;,::::::::::::::::::::::::
+ ,:; ## ## ##  #####     ####      ## ## ##  ##   ##  ;::
+ ,,; ## ## ##  ## ##    ##         ## ## ##  ##   ##  ;::
+ ,,; ## ## ##  ##  ##  ##   ####   ## ## ##   ## ##   ;::
+ ,,' ## ## ##  ## ##    ##    ##   ## ## ##   ## ##   :::
+ ,:: ########  ####      ######    ########    ###    :::
+ ,,,:,,:,,:::,,,:;:::::::::::::::;;;:::;:;:::::::::::::::
+ ,,,,,,,,,,,,,,,,,,,,,,,,:,::::::;;;;:::::;;;;::::;;;;:::
+                                                         
+	     (c) WDGWV. 2013, http://www.wdgwv.com           
+	 websites, Apps, Hosting, Services, Development.      
+
+  File Checked.
+  Checked by: WdG.
+  File created: WdG.
+  date: 07-06-2013
+
+  © WDGWV, www.wdgwv.com
+  All Rights Reserved.
+*/
+
+$dir = opendir('items');
+
+$template = file_get_contents("http://www.wdgwv.com/placeholder");
+
+$menu = array();
+
+while ( ( $file = readdir ( $dir ) ) !== false )
+{
+	if ($file != '.' && $file != '..')
+	{
+		echo "Generating " . $file . "\r\n";
+
+		ob_start();
+		include('items/' . $file);
+		$open = ob_get_contents();
+		ob_end_clean();
+
+		$open = preg_replace("#{BACK}#", "<a href='/index.html'>Back</a><hr /><br /><br />", $open);
+
+		$replace = $template;
+		$replace = preg_replace("#PLACEHOLDER#", $open, $replace);
+		$replace = preg_replace("#&nbsp;WDGWV#", "&nbsp;Open WDGWV", $replace);
+
+		$menu[] = substr($file, 0, -5);
+
+		$file = fopen($file, 'w');
+		@fwrite($file, $replace);
+		@fclose($file);
+	}
+}
+
+#FIX THE MENU
+	ob_start();
+	include('items/index.html');
+	$open = ob_get_contents();
+	ob_end_clean();
+	
+	$open    = preg_replace("#{BACK}#", "<a href='/index.html'>Back</a><hr /><br /><br />", $open);
+
+	$menuTMP = null;
+	for ($i=0; $i<sizeof($menu); $i++) 
+	{ 
+		$item = $menu[$i];
+		$item = preg_replace("/_/", "&nbsp;", $item);
+		
+		$menuTMP .= "<a href=\"{$menu[$i]}.html\">{$item}</a>";
+
+		if ($i < (sizeof($menu)-1) )
+			$menuTMP .= "&nbsp;&nbsp;||&nbsp;&nbsp;";
+	}
+
+	$open    = preg_replace("#{MENU}#", $menuTMP, $open);
+	$replace = $template;
+	$replace = preg_replace("#PLACEHOLDER#", $open, $replace);
+	$replace = preg_replace("#&nbsp;WDGWV#", "&nbsp;Open WDGWV", $replace);
+
+	$file = fopen('index.html', 'w');
+	@fwrite($file, $replace);
+	@fclose($file);
+
+echo "All Files Created!\r\n\tDon't Wait!!!\r\n\r\n\t\tUPLOAD IT!!!";
+?>
